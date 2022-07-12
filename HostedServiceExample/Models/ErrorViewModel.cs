e1fa4ea11bdcccf968e4c41b5e16ee42a3352a19
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-11 22:26:50 -0400

    Add project files.

---
+++ HostedServiceExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HostedServiceExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
